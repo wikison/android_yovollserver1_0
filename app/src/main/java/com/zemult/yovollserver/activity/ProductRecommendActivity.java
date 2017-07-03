bc3f6d1796350831501b359fb1d178b40a191b1b
Wikison <wikison@foxmail.com>
2017-07-03 15:52:41 +0800

    views

---
+++ app/src/main/java/com/zemult/yovollserver/activity/ProductRecommendActivity.java
@@ -0,0 +1,21 @@
+package com.zemult.yovollserver.activity;
+
+import com.zemult.yovollserver.R;
+import com.zemult.yovollserver.app.BaseActivity;
+
+/**
+ * Created by Wikison on 2017/7/3.
+ * 产品推荐
+ */
+
+public class ProductRecommendActivity extends BaseActivity {
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.activity_product_recommend);
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
